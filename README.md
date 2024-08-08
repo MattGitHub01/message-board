c4d3afcd51665c7bf4ee9dccac0978bf0c53dc62
MattGitHub01 <tuckermatthewcrandall@gmail.com>
2024-08-07 18:06:25 -0600

    Create new GitHub repo, copy files from deleted repo to this one

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# message-board
 A basic message board created with Node.js and Express.
+
+I was having trouble publishing the site on railway.app so I created a new repo and copied these files from the previous repo to hopefully publish the site effectively.
---
+++ routes/mainRoute.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+
+const messages = [
+    {
+        text: 'Howdy, partner!',
+        user: 'Cowboy Guy',
+        added: new Date()
+    },
+    {
+        text: 'Well howdy there stranger!',
+        user: 'Other Cowboy Guy',
+        added: new Date()
+    }
+];
+
+router.get('/', (req, res) => {
+    res.render('index', { title: 'Mini Messaging Web App', messages: messages });
+});
+
+router.get('/new', (req, res) => {
+    res.render('form', { title: 'New Message' });
+});
+
+router.post('/new', (req, res) => {
+    messages.push({
+        text: req.body.msgTxt,
+        user: req.body.usrName,
+        added: new Date(),
+    });
+    res.redirect('/');
+});
+
+module.exports = router;
